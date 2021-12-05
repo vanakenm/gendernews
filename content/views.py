31ddafb661a4be765cbf70427d43bba38688c87b
Martin Van Aken <martin@joyouscoding.com>
2021-12-05 13:07:30 +0100

    404 page - maybe

--- content/views.py
+++ content/views.py
@@ -12,6 +12,15 @@ def contact(request):
     context = {}
     return render(request, 'content/contact.html', context)
 
+def error_404(request, exception):
+    context = {}
+    return render(request, 'content/404.html', context)
+
+def error_500(request):
+    context = {}
+    return render(request, 'content/404.html', context)
+
+
 def send_message(request):
     context = {
         "notice": 'Message sent successfully!'
--- gendernews/urls.py
+++ gendernews/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +23,6 @@ urlpatterns = [
     path('', include('content.urls')),
 ]
 
+
+handler404 = 'content.views.error_404'
+handler500 = 'content.views.error_500'
